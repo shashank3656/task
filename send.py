4d51d9f89ce8f336e2fdc787b1794b2c7239c1f1
shashank3656 <79498546+shashank3656@users.noreply.github.com>
2022-10-08 00:54:51 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,23 +1,23 @@
-from flask import Flask
-from flask_mail import Mail, Message
+import email.mime.text
+import smtplib
 
-app = Flask(__name__)
-mail= Mail(app)
+username = 'nookala3656@gmail.com'
+password = 'pjuyhgioilukcyjq'
+hostname = 'smtp.gmail.com'
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'nookala3656@gmail.com'
-app.config['MAIL_PASSWORD'] = 'pjuyhgioilukcyjq'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+msg = email.mime.text.MIMEText('Your Website Server is down')
+msg['Subject'] = 'Server Down'
+msg['From'] = 'nookala3656@gmail.com'
+msg['To'] = 'shashankgowtham12@gmail.com'
 
-@app.route("/")
-def index():
-   msg = Message('Hello', sender = 'nookala3656@gmail.com', recipients = ['shashankgowtham12@gmail.com'])
-   msg.body = "Hello team server was down"
-   mail.send(msg)
-   return "Sent"
-
-if __name__ == '__main__':
-   app.run(debug = True, host='0.0.0.0', port=8000)
+server = smtplib.SMTP(hostname, 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username, password)
+server.sendmail(
+        'abc@gmail.com',
+        ['shashankgowtham12@gmail.com'],
+        msg.as_string(),
+        )
+server.close()
